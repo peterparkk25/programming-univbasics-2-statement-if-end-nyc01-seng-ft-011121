2c8b36ad0218dfb81f68157caa949f60cf8c494d
Peter Park 2.0 <peterparkk25+@gmail.com>
2020-12-25 21:17:28 +0000

    Automatically backed up by Learn

--- test_file.rb
+++ test_file.rb
@@ -1,12 +1,1 @@
-chance_of_rain = -23
-
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter!"
-
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Pack an umbrella!"
-
-else
-  puts "Stay home and read Hegel."
-  
-end
+current_time = Time.now
